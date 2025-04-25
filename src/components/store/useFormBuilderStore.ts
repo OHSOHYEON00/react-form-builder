4ac1c42455d293b387f457b315ee07857396001e
OHSOHYEON00 <ohsohyeon24@gmail.com>
2025-04-25 15:41:04 +0200

    chore: integrate Zustand Devtools

--- src/components/store/useFormBuilderStore.ts
+++ src/components/store/useFormBuilderStore.ts
@@ -1,26 +1,29 @@
 import { create } from "zustand";
 import { nanoid } from "nanoid";
 import { FormField } from "@/types/form";
+import { devtools } from "zustand/middleware";
 
 interface FormBuilderState {
   items: FormField[];
   addItem: (item: Partial<FormField>) => void;
 }
 
-export const useFormBuilderStore = create<FormBuilderState>((set) => ({
-  items: [],
-  addItem: (item) =>
-    set((state) => {
-      const newId = `${nanoid()}-${item.formItem}`;
-      return {
-        items: [
-          ...state.items,
-          {
-            ...item,
-            id: newId,
-            name: item.name || newId,
-          } as FormField,
-        ],
-      };
-    }),
-}));
+export const useFormBuilderStore = create<FormBuilderState>()(
+  devtools((set) => ({
+    items: [],
+    addItem: (item) =>
+      set((state) => {
+        const newId = `${nanoid()}-${item.formItem}`;
+        return {
+          items: [
+            ...state.items,
+            {
+              ...item,
+              id: newId,
+              name: item.name || newId,
+            } as FormField,
+          ],
+        };
+      }),
+  }))
+);
